c0b8b567106f53ce5106b05bfeb36bc2c727c8b1
Arnobi Zabaleta <arnobizabaleta@gmail.com>
2022-03-12 14:54:20 -0500

    Contando y buscando TODOs (Filtrando busqueda de los usuarios)

--- src/App.js
+++ src/App.js
@@ -6,22 +6,57 @@ import {TodoList} from './TodoList';
 import {TodoItem} from './TodoItem';
 import {CreateTodoButton} from './CreateTodoButton'
 // import './App.css';
-const todos = [
+const defaultTodos = [
   {text: 'Lavar mi ropa', completed:true},
-  {text: 'Tomar el curso de Intro a React', completed:false},
+  {text: 'Tomar el curso de Intro a React', completed:true},
   {text: 'Sacar a mi perro', completed:false},
   {text: 'Estudiar Backend', completed:false},
 
 ];
 function App(props) {
+  
+  const [todos,setTodos] = React.useState(defaultTodos);
+
+  const [searchValue, setSearchValue] = React.useState("");
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  /* todo.completed === true es lo mismo que !!todo.completed */
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if(!searchValue.length >= 1) {
+    searchedTodos = todos;
+  }else{
+    
+    searchedTodos = todos.filter(todo => {
+      //Pasando  el texto de cada TODO a minuscula
+      const todoText = todo.text.toLowerCase();
+      //Pasando  el inputText a minuscula
+      const searchText = searchValue.toLowerCase();
+      //Filtrando Todos de acuerdo al InputText
+      return todoText.includes(searchText);
+    });
+
+  }
+
   return (
     <React.Fragment>
-      <TodoCounter />
+      <TodoCounter 
+       total={totalTodos}
+       completed={completedTodos}
+
+      />
        
-      <TodoSearch /> 
+      <TodoSearch 
+      searchValue = {searchValue}
+      setSearchValue = {setSearchValue}
+     
+      
+      /> 
       
       <TodoList>
-        {todos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem 
           key={todo.text} 
           text={todo.text} 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./TodoSearch.css";
 
-function TodoSearch(){
-    const [searchValue, setSearchValue] = React.useState("");
-
+function TodoSearch({searchValue, setSearchValue}){
+    
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
         setSearchValue(event.target.value);
     };
     
-    return [
+    return (
         <input  className="TodoSearch" 
          placeholder="Cebolla"
          value={searchValue}
@@ -17,10 +16,9 @@ function TodoSearch(){
         
         >
         
-        </input>,
+        </input>
         
-        <p  >{searchValue}</p>
-    ];
+    );
   /*   React para renderizar necesita hacerlo con todos nuestros componetentes en una 
     etiqueta, o bien sea, podemos enviarle un array para realizar el render */
 }
