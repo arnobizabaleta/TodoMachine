71df1727382bbe12a486490df7a5952167d7ec6b
Arnobi Zabaleta <arnobizabaleta@gmail.com>
2022-03-26 09:02:21 -0500

    Agregando más comentarios a código de cloneElement y React.Children

--- src/TodoHeader/index.js
+++ src/TodoHeader/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 
 function TodoHeader({children, loading}){
-    
+    //No importa si viene un elemento, o dos o null siempre nos devuelve un array
     return (
         <header>
             {
             React.Children
             .toArray(children)
             .map(child => React.cloneElement(child, {loading}))
-                
+            //Por cada child vamos a llamar a clone element.
+            //React.cloneElement
+            //Crear elemento a partir de otro (elemento, objeto con las props que queramos que tenga)
                 
             };
         </header>
