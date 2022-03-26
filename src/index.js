af069472d9628b314c3a38966a2538706d2e297a
Arnobi Zabaleta <arnobizabaleta@gmail.com>
2022-03-26 09:50:26 -0500

    Primeras pasos con High Order Componenten

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 /* import './index.css'; */
-import App from './App/index.js';
+//import App from './App/index.js';
 
+//WrappedComponent Is App
+function App(props){
+  return(
+    <h1>¡{props.saludo}, {props.nombre}!</h1>
+  );
+}
 
+function withSaludo(WrappedComponent){
+  return function WrappedComponentWithSaludo(saludo) { //Función Intermedia
+    return function componenteDeVerdad(props){
+      return (
+        <React.Fragment>
+        <WrappedComponent {...props} saludo={saludo}/>
+        <p>Estamos acompañando al componente WrappedComponent</p>
+        </React.Fragment>
+      );
+    }
+    
+  }
+   
+  
+}
 
-
-
+const AppWithSaludo = withSaludo(App)("Hi");//Primer parametro(WrappedComponentName)
+//Segundo parametro(Propiedad que se envia a través de la función intermedia)
+// y es recibida y ejecuta por nuestro WrappedComonent App
 ReactDOM.render(
   
-  <App />,
+  <AppWithSaludo  nombre = "Juanita" />,
     
  /*  Componente App Indentificado por su inicio en Mayuscula */
  
